5105548b79a2ee6e54cf96a20213f94cdcea8df7
andmrqzv <83743411+andmrqzv@users.noreply.github.com>
2025-03-09 11:21:57 -0500

    Add files via upload
    Added an account registration page and modified login

--- src/pages/AccountRegestration.jsx
+++ src/pages/AccountRegestration.jsx
@@ -5,15 +5,14 @@ import React from "react";
 
 
 
-const Login = () => {
+const AccountSetting = () => {
     // const navigate = useNavigate();   //it is a hook, think of it as a routers that we can use to route it to different routes
 
     React.useEffect(() => {
-            window.location.href = "/assets/static/Login.html";
-        }, []);
+        window.location.href = "/assets/static/AccountRegestration.html";
+    }, []);
+
     return null;
 };
 
-
-export default Login;
-
+export default AccountSetting;
